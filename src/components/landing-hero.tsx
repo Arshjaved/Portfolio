317bb228ab4aadc598f47697c36342dccb1fb6c9
Arshiyan Javed <43108841+Arshjaved@users.noreply.github.com>
2026-05-07 03:46:49 +0200

    Update skills in landing hero component

--- src/components/landing-hero.tsx
+++ src/components/landing-hero.tsx
@@ -51,7 +51,7 @@ export default function LandingHero() {
                 Arshiyan Javed
               </h1>
               <span className="text-xl font-semibold text-zinc-900 dark:text-zinc-100 md:text-3xl">
-                Full Stack Developer | React, Next.js & Headless CMS
+                Full Stack Developer | React, Next.js, Node.js & Headless CMS
               </span>
             </FadeUp>
             <FadeUp key="description" duration={0.6} delay={0.2}>
