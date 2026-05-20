e45fe2e005b7ce174140cd4ecedf1d26996ea81e
Arshiyan Javed <43108841+Arshjaved@users.noreply.github.com>
2026-05-21 00:21:52 +0200

    Update copyright name in mobile-menu component

--- src/components/utility/mobile-menu.tsx
+++ src/components/utility/mobile-menu.tsx
@@ -58,7 +58,7 @@ export default function MobileMenu({
                 ))}
                 <ThemeSwitch setClose={setOpenMenu} />
               </div>
-              <div className="absolute bottom-0 py-6">©2026 Arshiyan Javed</div>
+              <div className="absolute bottom-0 py-6">©2026 Courtney Wilson</div>
             </Dialog.Panel>
           </Transition.Child>
         </div>
